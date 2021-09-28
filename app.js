0f0dcdc163822c38d1823a149f848154820b257f
nessrin-hadjidj <82982884+nessrin-hadjidj@users.noreply.github.com>
2021-09-28 18:12:39 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+function openNav (){
+    document.getElementById('sideNav').style.width = '270px'
+    document.getElementById('main').style.marginLeft = '250px'
+    document.getElementById('openNav').style.display = 'none'
+}
+
+function closeNav (){
+    document.getElementById('sideNav').style.width = '0px'
+    document.getElementById('main').style.marginLeft = '0px'
+    document.getElementById('main').style.marginLeft = '0px'
+    document.getElementById('openNav').style.display = 'block'
+    
+
+}
